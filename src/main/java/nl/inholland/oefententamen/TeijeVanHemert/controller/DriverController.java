6f4c141abb1f0ccd1150c15f86e408e16573375d
Teije <teijevhemert@gmail.com>
2020-10-27 13:40:17 +0100

    Completed Assignment 5a

--- src/main/java/nl/inholland/oefententamen/TeijeVanHemert/controller/DriverController.java
+++ src/main/java/nl/inholland/oefententamen/TeijeVanHemert/controller/DriverController.java
@@ -1,9 +1,15 @@
 package nl.inholland.oefententamen.TeijeVanHemert.controller;
 
 import lombok.extern.java.Log;
+import nl.inholland.oefententamen.TeijeVanHemert.model.Driver;
 import nl.inholland.oefententamen.TeijeVanHemert.service.DriverService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Log
 @Controller
@@ -17,5 +23,14 @@ public class DriverController
         this.driverService = driverService;
     }
 
-    
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity GetDriverById(@PathVariable long id)
+    {
+        Driver driver = driverService.GetDriverById(id);
+        log.info("--- Exam Question 5a completed ---");
+
+        return ResponseEntity
+                .status(200)
+                .body(driver);
+    }
 }
